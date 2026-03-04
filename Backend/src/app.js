bd70d44ebeedaba6ea8f53f2b5d2d2c5f64ac173
AnamikaPrajapati <Subiprajapati5@gmail.com>
2026-03-05 02:15:40 +0545

    Login & Register UI

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,9 +1,14 @@
 import express from 'express'
+import cors from 'cors'
 
 
 
 const app = express()
 app.use(express.json())
+app.use(cors({
+    origin:"http://localhost:5173", 
+    credentials:true
+}))
 
 // required all the routers here
 const authRouter = require("./routes/auth.routes")
